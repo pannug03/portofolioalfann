d2f3988994b2765f12d403a83d157bb565a90d82
pannug03 <152354222+pannug03@users.noreply.github.com>
2025-02-26 14:37:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    let btn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
